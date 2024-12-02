78be887dca1f24e9b92e4ab55ff4e70d033e7f59
dhanush <dhanushsbhargav@gmail.com>
2024-12-01 19:52:13 -0800

    resnet model

--- Code/dataloader.py
+++ Code/dataloader.py
@@ -2,9 +2,11 @@ import configparser
 import os
 import pandas as pd
 import torchvision
+import random
 import torch
 from torch.utils.data import Dataset, DataLoader
-from torchvision.transforms.v2 import ToTensor, Compose, ToImage, ToDtype
+from torchvision.transforms import InterpolationMode
+from torchvision.transforms.v2 import ToTensor, Compose, ToImage, ToDtype, Resize, CenterCrop
 from torchvision.io import read_video
 
 def prepare_file_paths(data_dir, violence_dir, nonviolence_dir):
@@ -29,22 +31,27 @@ def train_test_paths(all_paths, test_split, random_seed):
     return train_paths, test_paths
 
 class ViolentVideos(Dataset):
-    def __init__(self, annotations_file, transform=None):
+    def __init__(self, annotations_file, transform=None, clip_len=32):
         self.files_labels = pd.read_csv(annotations_file)
         self.transform = transform
+        self.clip_len = clip_len
 
     def __len__(self):
         return len(self.files_labels)
 
     def __getitem__(self, idx):
-        video = read_video(self.files_labels['path'][idx])
+        video, audio, metadata = read_video(self.files_labels['path'][idx], output_format="TCHW", pts_unit='sec')
         label = self.files_labels['label'][idx]
+        max_seek = video.shape[0] - self.clip_len
+        start = random.randint(0, max_seek)
+        vid = video[start:start + self.clip_len]
+
         if self.transform:
-            video = self.transform(video)
-        return video, label
+            vid = self.transform(vid)
+        return vid, torch.tensor(label, dtype=torch.float32)
 
 
-def get_data_loader():
+def get_data_loader(transforms, clip_len=32):
     config = configparser.ConfigParser()
     config.read('config.conf')
     test_file_path = config.get('Dataset', 'dataset_path') + os.path.sep + 'test.csv'
@@ -64,8 +71,10 @@ def get_data_loader():
         train.to_csv(train_file_path, index=False)
         test.to_csv(test_file_path, index=False)
 
-    train_dataset = ViolentVideos(train_file_path, transform=Compose([ToImage(), ToDtype(torch.float32, scale=True)]))
-    test_dataset = ViolentVideos(test_file_path, transform=Compose([ToImage(), ToDtype(torch.float32, scale=True)]))
+    train_dataset = ViolentVideos(train_file_path, transform=transforms, clip_len=clip_len)
+    test_dataset = ViolentVideos(test_file_path, transform=transforms, clip_len=clip_len)
+
+    video, label = train_dataset.__getitem__(1181)
 
     train_dataloader = DataLoader(train_dataset, batch_size=int(config.get('Dataset', 'batch_size')), shuffle=True)
     test_dataloader = DataLoader(test_dataset, batch_size=int(config.get('Dataset', 'batch_size')), shuffle=True)
@@ -73,4 +82,4 @@ def get_data_loader():
     return train_dataloader, test_dataloader
 
 if __name__ == '__main__':
-    train_loader, test_loader = get_data_loader()
+    train_loader, test_loader = get_data_loader(Compose([ToTensor()]))
