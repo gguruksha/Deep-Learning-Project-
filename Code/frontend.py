a13aed20dced8417279d7604b643c91a52423766
dhanush <dhanushsbhargav@gmail.com>
2024-12-07 23:44:51 -0500

    streamlit changes

--- Code/frontend.py
+++ Code/frontend.py
@@ -1,32 +1,31 @@
-
-### CHANGES
-
 import streamlit as st
 import os
 import torch
-from torchvision.transforms import Compose, Resize, ToTensor
-from Frame_Extraction import extract_frames  # Corrected import
-from video_resnet import ViolenceClassifier
 import shutil
-import cv2
-from PIL import Image
+from run_inference import get_clips, ViolenceClassifierInference
+import numpy as np
+
+
+model = ViolenceClassifierInference()
 
 # Streamlit UI setup
 st.set_page_config(page_title="Violence Detection", page_icon=":movie_camera:")
 
 # Set constants
 TEMP_VIDEO_PATH = "temp_video.mp4"
-TEMP_FRAMES_DIR = "temp_frames"
-MODEL_PATH = "model_resnet.pt"
-DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-# Ensure temp directories exist
-os.makedirs(TEMP_FRAMES_DIR, exist_ok=True)
 
 # Streamlit UI
 st.title("Violent vs Non-Violent Video Classification")
 
 uploaded_video = st.file_uploader("Upload a video (mp4 format)", type=["mp4"])
+
+def classify_video(path):
+    clips = get_clips(path)
+    output = model.infer(clips)
+    if np.max(output) > 0.5:
+        return "Violent"
+    else:
+        return "Non-Violent"
 
 if uploaded_video is not None:
     # Save uploaded video temporarily
@@ -37,7 +36,7 @@ if uploaded_video is not None:
 
     # Perform classification
     try:
-        result = classify_video(TEMP_VIDEO_PATH, model, DEVICE)
+        result = classify_video(TEMP_VIDEO_PATH)
         st.success(f"The video is classified as: **{result}**")
     except Exception as e:
         st.error(f"Error during video classification: {e}")
